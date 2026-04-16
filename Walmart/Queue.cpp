c8204264b037e13fe346718c98be976fcc578c7c
Goodroe <jgoodroe@una.edu>
2026-04-16 11:39:50 -0500

    binaryHeap

--- Walmart/Queue.cpp
+++ Walmart/Queue.cpp
@@ -33,24 +33,3 @@ bool Queue::IsEmpty()const {
 bool Queue::IsFull()const {
 	return back - front == MAX;
 }
-
-bool Queue::EnQ(int e) {
-	bool success = false;
-	if (!IsFull()) {
-		data[back % MAX] = e;
-		back++;
-		// data[back++ % MAX] = e;
-		success = true;
-	}
-	return success;
-}
-
-bool Queue::DeQ(int& e) {
-	bool success = false;
-	if (!IsEmpty()) {
-		e = data[front % MAX];
-		front++;
-		// e = data[front++ % MAX];
-		success = true;
-	}
-	return success;
