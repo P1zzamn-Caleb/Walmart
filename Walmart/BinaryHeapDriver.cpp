62b3885978b724f3ec60b8cb84d9fa9280aad2e9
chloegbyrd <chloegbyrd@gmail.com>
2026-04-19 14:44:31 -0500

    Remove comments and clean up BinaryHeapDriver.cpp

--- Walmart/BinaryHeapDriver.cpp
+++ Walmart/BinaryHeapDriver.cpp
@@ -1,4 +1,13 @@
-/*
+//**********************************************************************
+//Author: Chloe Byrd, Jay Goodroe, Caleb Ellis, and Tori Dean
+//Name: Binary Heap Test Driver
+//Purpose: Tests the BinaryHeap by reading graph data from a file,
+//        inserting adjacent vertices into the heap, and removing them 
+//        in priority order.
+//Incoming: File input (ScooterWeights.txt)
+//Outgoing: Shows output revealing inserted values and removal order
+//Return: int (Exit for the program status)
+//**********************************************************************
 #include <iostream>
 #include <fstream>
 #include "BinaryHeap.h"
@@ -7,6 +16,15 @@ using namespace std;
 
 const string FILE_NAME = "ScooterWeights.txt";
 
+//**********************************************************************
+//Name: Main (Binary Heap Test Driver)
+//Purpose: Tests the BinaryHeap by reading graph data from a file,
+//        inserting adjacent vertices into the heap, and removing them 
+//        in priority order.
+//Incoming: File input (ScooterWeights.txt)
+//Outgoing: Shows output revealing inserted values and removal order
+//Return: int (Exit for the program status)
+//**********************************************************************
 int main() {
     ifstream graphData;
     int numVertices = 0;
@@ -15,26 +33,42 @@ int main() {
     char marker;
     char throwAway;
 
+//**********************************************************************
+//OPENFILE
+//**********************************************************************
     graphData.open(FILE_NAME);
     if (!graphData.is_open()) {
         cout << FILE_NAME << ": Error opening file." << endl;
         return 1;
     }
 
+//**********************************************************************
+//SkipHeaderLines
+//**********************************************************************
     for (int i = 0; i < 6; i++) {
         graphData.get(throwAway);
         while (throwAway != '\n') {
             graphData.get(throwAway);
         }
     }
-
+    
+//**********************************************************************
+// ReadVertexCount
+//**********************************************************************
     graphData >> numVertices;
 
     cout << "WELCOME TO THE BINARY MIN HEAP TEST!" << endl;
 
+
+//**********************************************************************
+// InitializeHeap
+//**********************************************************************
     BinaryHeap heap;
     heap.MaxHeap();
 
+//**********************************************************************
+// InsertValuesIntoHeap
+//**********************************************************************
     for (int i = 0; i < numVertices; i++) {
         graphData >> thisVertice;
 
@@ -56,6 +90,9 @@ int main() {
     cout << endl;
     cout << "Removing values from min heap in priority order:" << endl;
 
+//**********************************************************************
+//RemoveValuesFromHeap
+//**********************************************************************
     int removedValue = heap.remove();
     while (removedValue != -1) {
         cout << removedValue << " ";
@@ -64,6 +101,9 @@ int main() {
 
     cout << endl;
 
+//**********************************************************************
+// Close File
+//**********************************************************************
     graphData.close();
     return 0;
 }
