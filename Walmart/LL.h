d62ff53b9f4a146f2f373bb47fa48fca6ed30d21
chloegbyrd <chloegbyrd@gmail.com>
2026-04-19 14:21:56 -0500

    Add documentation comments for Node and LL classes

--- Walmart/LL.h
+++ Walmart/LL.h
@@ -10,6 +10,14 @@
 #include <iostream>
 using namespace std;
 
+//*************************************************************************************
+//Name: Node
+//Purpose: This represents a single edge in the adjacency list, storing the connected
+//			vertex, edge weight, and pointer to the next node.
+//Incoming: int w (weight), int c (connected vertex), Node* node (next pointer)
+//Outgoing: Initializes a Node object
+//Return: none
+//**************************************************************************************
 class Node
 {
 public:
@@ -22,6 +30,14 @@ public:
 	friend class BinaryHeap;
 };
 
+//*************************************************************************************
+//Name: LL (Linked List)
+//Purpose: This represents a linked list used in the adjacency list to store all
+//			edges connected to a single vertex.
+//Incoming: none
+//Outgoing: Initializes an empty linked list
+//Return: none
+//**************************************************************************************
 class LL
 {
 public:
@@ -29,10 +45,23 @@ public:
 
 	LL() : head(nullptr) {};
 
+//*************************************************************************************
+//Name: Insert
+//Purpose: Inserts a new node at the beginning of the linked list
+//Incoming: int e (edge weight), int c (connected vertex)
+//Outgoing: Updates the linked list structure
+//Return: none
+//**************************************************************************************
 	void Insert(int e, int c);
+
+//*************************************************************************************
+//Name: Print
+//Purpose: Traverses and displays all nodes in the linked lsit
+//Incoming: None
+//Outgoing: Shows output of connections and weights
+//Return: none
+//**************************************************************************************
 	void Print()const;
 };
-
-
 
 #endif
