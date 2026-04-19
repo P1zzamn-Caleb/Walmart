4a80935ad119ecde0080de4685769c467a457f79
chloegbyrd <chloegbyrd@gmail.com>
2026-04-19 14:14:49 -0500

    Edit comments for Insert and Print functions
    Updated author and function names in comments for clarity.

--- Walmart/LL.cpp
+++ Walmart/LL.cpp
@@ -1,8 +1,8 @@
 #include "LL.h"
 
 //********************************************************************************
-// Author: Caleb Ellis
-// Name: InsertWeight
+// Author: Caleb Ellis (Editted by Chloe)
+// Name: Insert
 // Purpose: Inserts weights in order in LL
 // Incoming: int e
 // Outgoing: updated LL
@@ -15,10 +15,11 @@ void LL::Insert(int e, int c) {
 
 
 //********************************************************************************
-// Author: Caleb Ellis
-// Purpose: Prints LL
+// Author: Caleb Ellis (Editted by Chloe)
+// Name: Print
+// Purpose: Traverses and displays all nodes in the linked list 
 // Incoming: none
-// Outgoing: none
+// Outgoing: Shows output of connections and weights
 // Return: none
 //********************************************************************************
 void LL::Print()const
